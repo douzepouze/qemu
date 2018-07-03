e71d2adec0aa0bb146cc437d4acab7d4d182c4cf
Steffen Görtz <contrib@steffen-goertz.de>
2018-07-03 21:47:05 +0200

    WIP NVMC Test.
    Signed-off-by: Steffen Görtz <contrib@steffen-goertz.de>

--- tests/microbit-test.c
+++ tests/microbit-test.c
@@ -1,19 +1,21 @@
  /*
- * QTest testcase for Microbit board using the Nordic Semiconductor nRF51 SoC
+ * QTest testcase for Microbit board using the Nordic Semiconductor nRF51 SoC.
+ *
  *
  * Copyright (c) 2018 Steffen Görtz <contrib@steffen-goertz.de>
  */
 
 
 #include "qemu/osdep.h"
+#include "exec/hwaddr.h"
 #include "libqtest.h"
-//#include "qemu-common.h"
-//#include "qemu/sockets.h"
-//#include "qemu/iov.h"
-//#include "qemu/bitops.h"
 
 
-#define FLASH_SIZE          (256 * 1024)
+#define PAGE_SIZE           1024
+#define FLASH_SIZE          (256 * PAGE_SIZE)
+#define FLASH_BASE          0x00000000
+#define UICR_BASE           0x10001000
+#define UICR_SIZE           0x100
 #define NVMC_BASE           0x4001E000UL
 #define NVMC_READY          0x400
 #define NVMC_CONFIG         0x504
@@ -24,26 +26,82 @@
 #define NVMC_ERASEUICR      0x514
 
 
-static char tmp_path[] = "/tmp/qtest.microbit.XXXXXX";
+static void fill_and_erase(hwaddr base, hwaddr size, uint32_t address_reg)
+{
+    /* Fill memory */
+    writel(NVMC_BASE + NVMC_CONFIG, 0x01);
+    for (hwaddr i = 0; i < size; ++i) {
+        writeb(base + i, i);
+        g_assert_cmpuint(readb(base + i), ==, i & 0xFF);
+    }
+    writel(NVMC_BASE + NVMC_CONFIG, 0x00);
 
+    /* Erase Page */
+    writel(NVMC_BASE + NVMC_CONFIG, 0x02);
+    writel(NVMC_BASE + address_reg, base);
+    writel(NVMC_BASE + NVMC_CONFIG, 0x00);
 
-static void test_nrf51_nvmc(void) {
+    /* Check memory */
+    for (hwaddr i = 0; i < size; ++i) {
+        g_assert_cmpuint(readb(base + i), ==, 0xFF);
+    }
+}
+
+static void test_nrf51_nvmc(void)
+{
     uint32_t value;
-//    /* Test always ready */
-//    value = readl(NVMC_BASE + NVMC_READY);
-//    g_assert_cmpuint(value & 0x01, ==, 0x01);
+    /* Test always ready */
+    value = readl(NVMC_BASE + NVMC_READY);
+    g_assert_cmpuint(value & 0x01, ==, 0x01);
 
     /* Test write-read config register */
-    writel(NVMC_BASE+NVMC_CONFIG, 0x03);
-    value = readl(NVMC_BASE + NVMC_READY);
-    g_assert_cmpuint(value, ==, 0x03);
-//
-//    for (i = 0; i < BIN_SIZE; ++i) {
-//        memory_content[i] = qtest_readb(s, base_addr + i);
-//        g_assert_cmpuint(memory_content[i], ==, pre_store[i]);
-//    }
-//    qtest_memread(global_qtest, addr, data, size);
-//    g_assert_cmphex(buf[i], ==, (uint8_t)i);
+    writel(NVMC_BASE + NVMC_CONFIG, 0x03);
+    g_assert_cmpuint(readl(NVMC_BASE + NVMC_CONFIG), ==, 0x03);
+    writel(NVMC_BASE + NVMC_CONFIG, 0x00);
+    g_assert_cmpuint(readl(NVMC_BASE + NVMC_CONFIG), ==, 0x00);
+
+    /* TODO: Test implementation based alignment */
+
+    /* Test PCR0 */
+    fill_and_erase(FLASH_BASE, PAGE_SIZE, NVMC_ERASEPCR0);
+    fill_and_erase(FLASH_BASE + PAGE_SIZE, PAGE_SIZE, NVMC_ERASEPCR0);
+
+    /* Test PCR1 */
+    fill_and_erase(FLASH_BASE, PAGE_SIZE, NVMC_ERASEPCR1);
+    fill_and_erase(FLASH_BASE + PAGE_SIZE, PAGE_SIZE, NVMC_ERASEPCR1);
+
+    /* Erase all */
+    writel(NVMC_BASE + NVMC_CONFIG, 0x01);
+    for (hwaddr i = 0; i < FLASH_SIZE / 4; i++) {
+        writel(FLASH_BASE + i * 4, i);
+        g_assert_cmpuint(readl(FLASH_BASE + i * 4), ==, i);
+    }
+    writel(NVMC_BASE + NVMC_CONFIG, 0x00);
+
+    writel(NVMC_BASE + NVMC_CONFIG, 0x02);
+    writel(NVMC_BASE + NVMC_ERASEALL, 0x01);
+    writel(NVMC_BASE + NVMC_CONFIG, 0x00);
+
+    for (hwaddr i = 0; i < FLASH_SIZE / 4; i++) {
+        g_assert_cmpuint(readl(FLASH_BASE + i * 4), ==, 0xFFFFFFFF);
+    }
+
+    /* Erase UICR */
+    writel(NVMC_BASE + NVMC_CONFIG, 0x01);
+    for (hwaddr i = 0; i < UICR_SIZE / 4; i++) {
+        writel(UICR_BASE + i * 4, i);
+        g_assert_cmpuint(readl(UICR_BASE + i * 4), ==, i);
+    }
+    writel(NVMC_BASE + NVMC_CONFIG, 0x00);
+
+    writel(NVMC_BASE + NVMC_CONFIG, 0x02);
+    writel(NVMC_BASE + NVMC_ERASEUICR, 0x01);
+    writel(NVMC_BASE + NVMC_CONFIG, 0x00);
+
+    for (hwaddr i = 0; i < UICR_SIZE / 4; i++) {
+        g_assert_cmpuint(readl(UICR_BASE + i * 4), ==, 0xFFFFFFFF);
+    }
+
 }
 
 
@@ -51,15 +109,8 @@ static void test_nrf51_nvmc(void) {
 int main(int argc, char **argv)
 {
     int ret;
-    int fd;
 
     g_test_init(&argc, &argv, NULL);
-
-    fd = mkstemp(tmp_path);
-    g_assert(fd >= 0);
-    ret = ftruncate(fd, FLASH_SIZE);
-    g_assert(ret == 0);
-    close(fd);
 
     global_qtest = qtest_startf("-machine microbit");
 
@@ -68,6 +119,5 @@ int main(int argc, char **argv)
     ret = g_test_run();
 
     qtest_quit(global_qtest);
-//    unlink(tmp_path);
     return ret;
 }
