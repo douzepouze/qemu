93dfc58000639ccff689c3770b43b54448935018
Steffen Görtz <contrib@steffen-goertz.de>
2018-07-17 14:52:20 +0200

    Julia UART v2
    Signed-off-by: Steffen Görtz <contrib@steffen-goertz.de>

--- hw/char/nrf51_uart.c
+++ hw/char/nrf51_uart.c
@@ -1,6 +1,10 @@
 /*
  * nRF51 SoC UART emulation
  *
+ * See nRF51 Series Reference Manual, "29 Universal Asynchronous
+ * Receiver/Transmitter" for hardware specifications:
+ * http://infocenter.nordicsemi.com/pdf/nRF51_RM_v3.0.pdf
+ *
  * Copyright (c) 2018 Julia Suvorova <jusual@mail.ru>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -12,8 +16,7 @@
 #include "qemu/log.h"
 #include "hw/registerfields.h"
 #include "hw/char/nrf51_uart.h"
-
-#define NRF51_UART_SIZE       0x1000
+#include "trace.h"
 
 REG32(STARTRX, 0x000)
 REG32(STOPRX, 0x004)
@@ -50,14 +53,14 @@ REG32(CONFIG, 0x56C)
 
 static void nrf51_uart_update_irq(Nrf51UART *s)
 {
-    unsigned int irq = 0;
+    bool irq = false;
 
-    irq = irq || (s->reg[A_RXDRDY] && (s->reg[A_INTEN] & R_INTEN_RXDRDY_MASK));
-    irq = irq || (s->reg[A_TXDRDY] && (s->reg[A_INTEN] & R_INTEN_TXDRDY_MASK));
-    irq = irq || (s->reg[A_ERROR]  && (s->reg[A_INTEN] & R_INTEN_ERROR_MASK));
-    irq = irq || (s->reg[A_RXTO]   && (s->reg[A_INTEN] & R_INTEN_RXTO_MASK));
+    irq |= (s->reg[R_RXDRDY] && (s->reg[R_INTEN] & R_INTEN_RXDRDY_MASK));
+    irq |= (s->reg[R_TXDRDY] && (s->reg[R_INTEN] & R_INTEN_TXDRDY_MASK));
+    irq |= (s->reg[R_ERROR]  && (s->reg[R_INTEN] & R_INTEN_ERROR_MASK));
+    irq |= (s->reg[R_RXTO]   && (s->reg[R_INTEN] & R_INTEN_RXTO_MASK));
 
-    qemu_set_irq(s->irq, !!irq);
+    qemu_set_irq(s->irq, irq);
 }
 
 static uint64_t uart_read(void *opaque, hwaddr addr, unsigned int size)
@@ -68,23 +71,27 @@ static uint64_t uart_read(void *opaque, hwaddr addr, unsigned int size)
     switch (addr) {
     case A_RXD:
         r = s->rx_fifo[s->rx_fifo_pos];
-        if (s->rx_fifo_len > 0) {
+        if (s->rx_started && s->rx_fifo_len) {
+            qemu_chr_fe_accept_input(&s->chr);
             s->rx_fifo_pos = (s->rx_fifo_pos + 1) % UART_FIFO_LENGTH;
             s->rx_fifo_len--;
-            qemu_chr_fe_accept_input(&s->chr);
+            if (s->rx_fifo_len) {
+                s->reg[R_RXDRDY] = 1;
+                nrf51_uart_update_irq(s);
+            }
         }
         break;
-
     case A_INTENSET:
     case A_INTENCLR:
     case A_INTEN:
-        r = s->reg[A_INTEN];
+        r = s->reg[R_INTEN];
         break;
     default:
-        r = s->reg[addr];
+        r = s->reg[addr / 4];
         break;
     }
 
+    trace_nrf51_uart_read(addr, r);
     return r;
 }
 
@@ -92,21 +99,26 @@ static gboolean uart_transmit(GIOChannel *chan, GIOCondition cond, void *opaque)
 {
     Nrf51UART *s = NRF51_UART(opaque);
     int r;
+    uint8_t c = s->reg[R_TXD];
 
     s->watch_tag = 0;
 
-    r = qemu_chr_fe_write(&s->chr, (uint8_t *) &s->reg[A_TXD], 1);
+    r = qemu_chr_fe_write(&s->chr, &c, 1);
     if (r <= 0) {
         s->watch_tag = qemu_chr_fe_add_watch(&s->chr, G_IO_OUT | G_IO_HUP,
                                              uart_transmit, s);
         if (!s->watch_tag) {
+            /* The hardware has no transmit error reporting,
+             * so silently drop the byte
+             */
             goto buffer_drained;
         }
         return FALSE;
     }
 
 buffer_drained:
-    s->reg[A_TXDRDY] = 1;
+    s->reg[R_TXDRDY] = 1;
+    s->pending_tx_byte = false;
     nrf51_uart_update_irq(s);
     return FALSE;
 }
@@ -124,22 +136,61 @@ static void uart_write(void *opaque, hwaddr addr,
 {
     Nrf51UART *s = NRF51_UART(opaque);
 
+    trace_nrf51_uart_write(addr, value);
+
     switch (addr) {
     case A_TXD:
-        s->reg[A_TXD] = value;
-        uart_transmit(NULL, G_IO_OUT, s);
+        if (!s->pending_tx_byte && s->tx_started) {
+            s->reg[R_TXD] = value;
+            s->pending_tx_byte = true;
+            uart_transmit(NULL, G_IO_OUT, s);
+        }
         break;
     case A_INTENSET:
-        s->reg[A_INTEN] |= value;
+        s->reg[R_INTEN] |= value;
         break;
     case A_INTENCLR:
-        s->reg[A_INTEN] &= ~value;
+        s->reg[R_INTEN] &= ~value;
         break;
-    case A_CTS ... A_RXTO:
+    case A_TXDRDY ... A_RXTO:
         s->reg[addr] = value;
         nrf51_uart_update_irq(s);
+        break;
+    case A_ERRORSRC:
+        s->reg[addr] &= ~value;
+        break;
+    case A_RXD:
+        break;
+    case A_RXDRDY:
+        if (value == 0) {
+            s->reg[R_RXDRDY] = 0;
+        }
+        nrf51_uart_update_irq(s);
+        break;
+    case A_STARTTX:
+        if (value == 1) {
+            s->tx_started = true;
+        }
+        break;
+    case A_STARTRX:
+        if (value == 1) {
+            s->rx_started = true;
+        }
+        break;
+    case A_SUSPEND:
+    case A_STOPTX:
+        if (value == 1) {
+            s->tx_started = false;
+        }
+    case A_STOPRX:
+        if (addr != A_STOPTX && value == 1) {
+            s->rx_started = false;
+            s->reg[R_RXTO] = 1;
+            nrf51_uart_update_irq(s);
+        }
+        break;
     default:
-        s->reg[addr] = value;
+        s->reg[addr / 4] = value;
         break;
     }
 }
@@ -154,27 +205,41 @@ static void nrf51_uart_reset(DeviceState *dev)
 {
     Nrf51UART *s = NRF51_UART(dev);
 
+    s->pending_tx_byte = 0;
+
     uart_cancel_transmit(s);
 
     memset(s->reg, 0, sizeof(s->reg));
 
+    s->reg[R_PSELRTS] = 0xFFFFFFFF;
+    s->reg[R_PSELTXD] = 0xFFFFFFFF;
+    s->reg[R_PSELCTS] = 0xFFFFFFFF;
+    s->reg[R_PSELRXD] = 0xFFFFFFFF;
+    s->reg[R_BAUDRATE] = 0x4000000;
+
     s->rx_fifo_len = 0;
     s->rx_fifo_pos = 0;
+    s->rx_started = false;
+    s->tx_started = false;
 }
 
 static void uart_receive(void *opaque, const uint8_t *buf, int size)
 {
 
    Nrf51UART *s = NRF51_UART(opaque);
+   int i;
 
-   if (s->rx_fifo_len >= UART_FIFO_LENGTH) {
+   if (size == 0 || s->rx_fifo_len >= UART_FIFO_LENGTH) {
         return;
     }
 
-    s->rx_fifo[(s->rx_fifo_pos + s->rx_fifo_len) % UART_FIFO_LENGTH] = *buf;
-    s->rx_fifo_len++;
+    for (i = 0; i < size; i++) {
+        uint32_t pos = (s->rx_fifo_pos + s->rx_fifo_len) % UART_FIFO_LENGTH;
+        s->rx_fifo[pos] = buf[i];
+        s->rx_fifo_len++;
+    }
 
-    s->reg[A_RXDRDY] = 1;
+    s->reg[R_RXDRDY] = 1;
     nrf51_uart_update_irq(s);
 }
 
@@ -182,7 +247,18 @@ static int uart_can_receive(void *opaque)
 {
     Nrf51UART *s = NRF51_UART(opaque);
 
-    return (s->rx_fifo_len < sizeof(s->rx_fifo));
+    return UART_FIFO_LENGTH - s->rx_fifo_len;
+}
+
+static void uart_event(void *opaque, int event)
+{
+    Nrf51UART *s = NRF51_UART(opaque);
+
+    if (event == CHR_EVENT_BREAK) {
+        s->reg[R_ERRORSRC] |= 3;
+        s->reg[R_ERROR] = 1;
+        nrf51_uart_update_irq(s);
+    }
 }
 
 static void nrf51_uart_realize(DeviceState *dev, Error **errp)
@@ -190,7 +266,7 @@ static void nrf51_uart_realize(DeviceState *dev, Error **errp)
     Nrf51UART *s = NRF51_UART(dev);
 
     qemu_chr_fe_set_handlers(&s->chr, uart_can_receive, uart_receive,
-                             NULL, NULL, s, NULL, true);
+                             uart_event, NULL, s, NULL, true);
 }
 
 static void nrf51_uart_init(Object *obj)
@@ -198,11 +274,38 @@ static void nrf51_uart_init(Object *obj)
     Nrf51UART *s = NRF51_UART(obj);
     SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
 
-    memory_region_init_io(&s->iomem, obj, &uart_ops, s,
-            "nrf51_soc.uart", NRF51_UART_SIZE);
-    sysbus_init_mmio(sbd, &s->iomem);
-    qdev_init_gpio_out_named(DEVICE(s), &s->irq, "irq", 1);
+    memory_region_init_io(&s->mmio, obj, &uart_ops, s,
+                          "nrf51_soc.uart", UART_SIZE);
+    sysbus_init_mmio(sbd, &s->mmio);
+    sysbus_init_irq(sbd, &s->irq);
 }
+
+static int nrf51_uart_post_load(void *opaque, int version_id)
+{
+    Nrf51UART *s = NRF51_UART(opaque);
+
+    if (s->pending_tx_byte) {
+        s->watch_tag = qemu_chr_fe_add_watch(&s->chr, G_IO_OUT | G_IO_HUP,
+                                             uart_transmit, s);
+    }
+
+    return 0;
+}
+
+static const VMStateDescription nrf51_uart_vmstate = {
+    .name = "nrf51_soc.uart",
+    .post_load = nrf51_uart_post_load,
+    .fields = (VMStateField[]) {
+        VMSTATE_UINT32_ARRAY(reg, Nrf51UART, 0x56C),
+        VMSTATE_UINT8_ARRAY(rx_fifo, Nrf51UART, UART_FIFO_LENGTH),
+        VMSTATE_UINT32(rx_fifo_pos, Nrf51UART),
+        VMSTATE_UINT32(rx_fifo_len, Nrf51UART),
+        VMSTATE_BOOL(rx_started, Nrf51UART),
+        VMSTATE_BOOL(tx_started, Nrf51UART),
+        VMSTATE_BOOL(pending_tx_byte, Nrf51UART),
+        VMSTATE_END_OF_LIST()
+    }
+};
 
 static Property nrf51_uart_properties[] = {
     DEFINE_PROP_CHR("chardev", Nrf51UART, chr),
@@ -216,6 +319,7 @@ static void nrf51_uart_class_init(ObjectClass *klass, void *data)
     dc->reset = nrf51_uart_reset;
     dc->realize = nrf51_uart_realize;
     dc->props = nrf51_uart_properties;
+    dc->vmsd = &nrf51_uart_vmstate;
 }
 
 static const TypeInfo nrf51_uart_info = {
--- include/hw/char/nrf51_uart.h
+++ include/hw/char/nrf51_uart.h
@@ -6,13 +6,6 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 or
  * (at your option) any later version.
- *
- * QEMU interface:
- * + sysbus MMIO regions 0: Memory Region with tasks, events and registers
- *   to be mapped to the peripherals instance address by the SOC.
- * + Named GPIO output "irq": Interrupt line of the peripheral. Must be
- *   connected to the respective peripheral interrupt line of the NVIC.
- *
  */
 
 #ifndef NRF51_UART_H
@@ -22,14 +15,16 @@
 #include "chardev/char-fe.h"
 
 #define UART_FIFO_LENGTH 6
+#define UART_BASE 0x40002000
+#define UART_SIZE 0x1000
 
 #define TYPE_NRF51_UART "nrf51_soc.uart"
 #define NRF51_UART(obj) OBJECT_CHECK(Nrf51UART, (obj), TYPE_NRF51_UART)
 
 typedef struct Nrf51UART {
     SysBusDevice parent_obj;
-    MemoryRegion iomem;
 
+    MemoryRegion mmio;
     CharBackend chr;
     qemu_irq irq;
     guint watch_tag;
@@ -38,7 +33,11 @@ typedef struct Nrf51UART {
     unsigned int rx_fifo_pos;
     unsigned int rx_fifo_len;
 
-    uint32_t reg[0x1000];
+    uint32_t reg[0x56C];
+
+    bool rx_started;
+    bool tx_started;
+    bool pending_tx_byte;
 } Nrf51UART;
 
 #endif
