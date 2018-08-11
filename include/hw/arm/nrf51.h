64313086820838b5937b9bdcf13beabe1c557f32
Steffen Görtz <contrib@steffen-goertz.de>
2018-08-11 10:42:00 +0200

    arm: Add header to hosts common definition for nRF51 SOC peripherals
    Adds a header that provides definitions that are used
    across nRF51 peripherals
    
    Signed-off-by: Steffen Görtz <contrib@steffen-goertz.de>

---
+++ include/hw/arm/nrf51.h
@@ -0,0 +1,24 @@
+/*
+ * Nordic Semiconductor nRF51 Series SOC Common Defines
+ *
+ * This file hosts generic defines used in various nRF51 peripheral devices.
+ *
+ * Reference Manual: http://infocenter.nordicsemi.com/pdf/nRF51_RM_v3.0.pdf
+ * Product Spec: http://infocenter.nordicsemi.com/pdf/nRF51822_PS_v3.1.pdf
+ *
+ * Copyright 2018 Steffen Görtz <contrib@steffen-goertz.de>
+ *
+ * This code is licensed under the GPL version 2 or later.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#ifndef NRF51_H
+#define NRF51_H
+
+/* Trigger */
+#define NRF51_TRIGGER_TASK 0x01
+
+/* Events */
+#define NRF51_EVENT_CLEAR  0x00
+
+#endif
